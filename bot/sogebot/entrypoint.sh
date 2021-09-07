0bf92cf9df523be7b1c93b655c0ec4288fdcfbfe
gOOvER <info@goover.de>
2021-09-07 09:19:53 +0200

    npm install

--- bot/sogebot/entrypoint.sh
+++ bot/sogebot/entrypoint.sh
@@ -8,12 +8,6 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 node -v
 npm -v
 
-# Update to latest npm
-npm install -g npm@latest
-
-# Install sqlite3
-npm install better-sqlite3 --save
-
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
