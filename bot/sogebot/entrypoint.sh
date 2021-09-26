f24de64a45dc6a67430b7d9887d8a11792437068
gOOvER <info@goover.de>
2021-09-26 08:39:42 +0200

    small fixes

--- bot/sogebot/entrypoint.sh
+++ bot/sogebot/entrypoint.sh
@@ -4,8 +4,15 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+echo "-------------------------------------------------------------"
+echo "Update npm to latest and update node packages. Please wait..."
+echo "-------------------------------------------------------------"
+npm install -g npm@latest; npm cache clean --force; npm update; npm install
+
 # Print Node.js Version
+echo "Installed Node Version:"
 node -v
+echo "Installed NPM Version:"
 npm -v
 
 # Replace Startup Variables
