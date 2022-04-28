868eaef3e3eb715dc507d7fe7412eebb37889208
gOOvER <info@goover.de>
2022-04-28 17:13:33 +0200

    Update entrypoint.sh

--- games/interstellarrift/entrypoint.sh
+++ games/interstellarrift/entrypoint.sh
@@ -39,46 +39,9 @@ fi
 # Install necessary to run packages
 echo "First launch will throw some errors. Ignore them"
 
-mkdir -p $WINEPREFIX
-
-# Check if wine-gecko required and install it if so
-if [[ $WINETRICKS_RUN =~ gecko ]]; then
-        echo "Installing Gecko"
-        WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
-
-        if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
-                wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.2/wine_gecko-2.47.2-x86.msi
-        fi
-
-        if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
-                wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.2/wine_gecko-2.47.2-x86_64.msi
-        fi
-
-        wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
-        wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
-fi
-
-# Check if wine-mono required and install it if so
-if [[ $WINETRICKS_RUN =~ mono ]]; then
-        echo "Installing mono"
-        WINETRICKS_RUN=${WINETRICKS_RUN/mono}
-
-        if [ ! -f "$WINEPREFIX/mono.msi" ]; then
-                wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/7.2.0/wine-mono-7.2.0-x86.msi
-        fi
-
-        wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
-fi
-
-# List and install other packages
-for trick in $WINETRICKS_RUN; do
-        echo "Installing $trick"
-        winetricks -q $trick
-done
-
 # install dotnet48 & vc2012
-winetricks -f dotnet48
 winetricks -f vcrun2015
+winetricks -f mono
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
